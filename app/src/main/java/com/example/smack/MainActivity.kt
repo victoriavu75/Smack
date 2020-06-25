613ea71231dc810b7a8b621b2144ae6669b7a4b8
Vu <victoriavu75@gmail.com>
2020-06-24 17:20:42 -0700

    lesson 3

--- app/src/main/java/com/example/smack/MainActivity.kt
+++ app/src/main/java/com/example/smack/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.smack
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
@@ -14,6 +15,7 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.example.smack.R.id
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,26 +24,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        val toolbar: Toolbar = findViewById(id.toolbar)
         setSupportActionBar(toolbar)
 
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val drawerLayout: DrawerLayout = findViewById(id.drawer_layout)
+        val navView: NavigationView = findViewById(id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
+        val navController = findNavController(id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     fun loginBtnNavClicked(view: View){
 
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
     }
 
     fun addChannelBtnClicked(view: View){
@@ -51,4 +57,5 @@ class MainActivity : AppCompatActivity() {
     fun sendMsgBtnClicked(view: View){
 
     }
+
 }
